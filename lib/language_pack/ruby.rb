4ac8cf00ccc9b174bcf8f2320e24cf2a07f81d2f
Dante Cassanego <dante.cassanego@gmail.com>
2018-11-16 11:24:56 -0800

    iterate

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -702,7 +702,8 @@ WARNING
           puts "Running: #{bundle_command}"
           instrument "ruby.bundle_install" do
             bundle_time = Benchmark.realtime do
-              pipe("bundle config build.ruby-odbc #{ENV['BUNDLE_BUILD__RUBY_ODBC']}")
+              bundler_output << pipe("bundle config build.ruby-odbc #{ENV['BUNDLE_BUILD__RUBY_ODBC']}")
+              bundler_output << pipe("bundle config")
               bundler_output << pipe("#{bundle_command} --no-clean", out: "2>&1", env: env_vars, user_env: true)
             end
           end
